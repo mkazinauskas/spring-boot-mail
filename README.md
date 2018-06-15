cb5dee52eadd1eaff97c57bcea5d7214f7da4749
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-06-15 16:30:33 +0300

    Sending html email and checking in browser

--- README.md
+++ README.md
@@ -2,28 +2,9 @@
 Spring boot mail sample
 
 # How to run
-
 1. `docker-compose up -d`
 2. `./gradlew bootRun`
-3. Check sent mails: `docker volume ls -q | docker volume inspect $(grep smtp_data)` and go to `"Mountpont"` directory
-Example command output:
-```$xslt
-[root@localhost]# docker volume ls |  docker volume inspect $(grep smtp_data)
-[
-    {
-        "CreatedAt": "2018-06-15T09:16:41+03:00",
-        "Driver": "local",
-        "Labels": {
-            "com.docker.compose.project": "springbootmail",
-            "com.docker.compose.volume": "smtp_data"
-        },
-        "Mountpoint": "/var/lib/docker/volumes/springbootmail_smtp_data/_data",
-        "Name": "springbootmail_smtp_data",
-        "Options": {},
-        "Scope": "local"
-    }
-]
-```
+3. Check sent mails in: [http://localhost:1080](http://localhost:1080)
 
 # How to stop
 1. `docker-compose down`
--- src/main/java/com/modzo/mail/MailApplication.java
+++ src/main/java/com/modzo/mail/MailApplication.java
@@ -1,13 +1,22 @@
 package com.modzo.mail;
 
+import com.samskivert.mustache.Mustache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheResourceTemplateLoader;
 import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.Reader;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class MailApplication {
@@ -19,19 +28,41 @@ public class MailApplication {
     }
 
     @Bean
-    CommandLineRunner command(JavaMailSender javaMailSender) {
-        return (args) -> {
-                sendMail(javaMailSender);
-        };
+    CommandLineRunner command(JavaMailSender javaMailSender,
+                              Mustache.Compiler compiler,
+                              MustacheResourceTemplateLoader templateLoader) {
+        return (args) -> sendMail(javaMailSender, compiler, templateLoader);
+
     }
 
-    private void sendMail(JavaMailSender javaMailSender) {
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setFrom("from@someuser.com");
-        mail.setTo("to@mail.com");
-        mail.setSubject("Test Mail");
-        mail.setText("Mail body");
-        javaMailSender.send(mail);
+    private void sendMail(JavaMailSender javaMailSender,
+                          Mustache.Compiler compiler,
+                          MustacheResourceTemplateLoader templateLoader) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+            helper.setFrom("from@mymail.com");
+            helper.setTo("to@myMail.com");
+            helper.setSubject("Sample message");
+            helper.setText(body(compiler, templateLoader), true);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
         logger.info("Mail sent!");
+    }
+
+    private String body(Mustache.Compiler compiler,
+                        MustacheResourceTemplateLoader templateLoader) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("time", LocalDateTime.now().toString());
+
+        try {
+            Reader template = templateLoader.getTemplate("mail-message");
+            return compiler.compile(template).execute(model);
+        } catch (Exception exception) {
+            throw new RuntimeException(exception.getMessage());
+        }
     }
 }
