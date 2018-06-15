c48f0f87e41f1750354cc1b7fce6574408550cf8
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-06-15 09:35:13 +0300

    Working sample with mail sender

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # spring-boot-mail
 Spring boot mail sample
+
+# How to run
+
+1. `docker-compose up -d`
+2. `./gradlew bootRun`
+3. Check sent mails: `docker volume ls -q | docker volume inspect $(grep smtp_data)` and go to `"Mountpont"` directory
+Example command output:
+```$xslt
+[root@localhost]# docker volume ls |  docker volume inspect $(grep smtp_data)
+[
+    {
+        "CreatedAt": "2018-06-15T09:16:41+03:00",
+        "Driver": "local",
+        "Labels": {
+            "com.docker.compose.project": "springbootmail",
+            "com.docker.compose.volume": "smtp_data"
+        },
+        "Mountpoint": "/var/lib/docker/volumes/springbootmail_smtp_data/_data",
+        "Name": "springbootmail_smtp_data",
+        "Options": {},
+        "Scope": "local"
+    }
+]
+```
+
+# How to stop
+1. `docker-compose down`
--- src/main/java/com/modzo/mail/MailApplication.java
+++ src/main/java/com/modzo/mail/MailApplication.java
@@ -1,12 +1,37 @@
 package com.modzo.mail;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
 public class MailApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MailApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(MailApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner command(JavaMailSender javaMailSender) {
+        return (args) -> {
+                sendMail(javaMailSender);
+        };
+    }
+
+    private void sendMail(JavaMailSender javaMailSender) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setFrom("from@someuser.com");
+        mail.setTo("to@mail.com");
+        mail.setSubject("Test Mail");
+        mail.setText("Mail body");
+        javaMailSender.send(mail);
+        logger.info("Mail sent!");
+    }
 }
